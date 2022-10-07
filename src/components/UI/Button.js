e1d1ad4365cd172d95ad13881274b287761b0ed8
AbdulSami10 <sami.sayani10@gmail.com>
2022-10-07 10:24:14 -0700

    work has been done

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import classes from './Button.module.css';
+import './Button.css';
 
 const Button = (props) => {
   return (
-    <button className={classes.button} type={props.type || 'button'} onClick={props.onClick}>
+    <button className="button" type={props.type || 'button'} onClick={props.onClick}>
       {props.children}
     </button>
   );
